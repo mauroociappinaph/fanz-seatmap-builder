80f99cd305a18a8a23d39a5481f9a090a736fb09
Mauro Ciappina <ciappinamaurooj@gmail.com>
2026-02-26 20:03:04 -0300

    refactor: complete missing barrel files and fix type errors

---
+++ src/components/ui/inspector/index.ts
@@ -0,0 +1,4 @@
+export * from "./GeneralSection";
+export * from "./LayoutSection";
+export * from "./SpecificsSection";
+export * from "./BulkLabelingSection";
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,1 +1,5 @@
+export * from "./domain";
+export * from "./factory";
+export * from "./labeling";
+export * from "./layout";
 export * from "./persistence";
